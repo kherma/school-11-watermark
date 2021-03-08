63f0a6e3ceb51e90b8f3930d8c97479715fd66f4
kherma <kamil.herma01@gmail.com>
2021-03-08 13:02:49 +0100

    Add image watermark to image

--- app.js
+++ app.js
@@ -13,11 +13,32 @@ const addTextWatermarkToImage = async (inputFile, outputFile, text) => {
   await image.quality(100).writeAsync(outputFile);
 };
 
+const addImageWatermarkToImage = async (
+  inputFile,
+  outputFile,
+  watermarkFile
+) => {
+  const image = await Jimp.read(inputFile);
+  const watermark = await Jimp.read(watermarkFile);
+  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  image.composite(watermark, x, y, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.5,
+  });
+  await image.quality(100).writeAsync(outputFile);
+};
+
 const initApp = () => {
   addTextWatermarkToImage(
     "./imageSource/test.jpg",
     "./test-with-watermark.jpg",
     "Hello world"
+  );
+  addImageWatermarkToImage(
+    "./imageSource/test.jpg",
+    "./test-with-watermark2.jpg",
+    "./imageSource/logo.png"
   );
 };
 
